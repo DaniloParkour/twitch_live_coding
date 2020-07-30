f70570b29bbea9e17708794454de088b05102104
DaniloParkour <danilomlcarvalho@gmail.com>
2020-07-30 19:44:54 -0300

    FinishLive

---
+++ Assets/Scripts/CharacterMoviment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class CharacterMoviment : MonoBehaviour
+{
+
+    public float Speed;
+    public int Force;
+
+    private Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody2D.velocity = new Vector2(-Speed, rigidbody2D.velocity.y);
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody2D.velocity = new Vector2(Speed, rigidbody2D.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0);
+            rigidbody2D.AddForce(new Vector2(0, Force));
+        }
+
+    }
+}
